c6e97f08404c8a4a094d996843899595a816df03
Tefached95 <mrqoqlobo@gmail.com>
2023-09-28 18:20:32 +0200

    main: Slight stylistic rewrite

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,10 @@ fn main() -> Result<(), String> {
 
     if args.len() != 3 {
         return Err(String::from(
-            format!("Invalid usage.\nUsage: `cargo run <path/to/input.ch8> <path/to/output/dir>`\nThe program takes a `.ch8` file, and outputs an `.asm` file with the same base name as the input.\n\tExample: `cargo run /home/me/Documents/maze.ch8 /home/me/Documents/` will produce a `maze.asm` file in `/home/me/Documents`"),
+            r#"Invalid usage.
+            Usage: `cargo run <path/to/input.ch8> <path/to/output/dir>`
+            The program takes a `.ch8` file, and outputs an `.asm` file with the same base name as the input.
+            Example: `cargo run /home/me/Documents/maze.ch8 /home/me/Documents/` will produce a `maze.asm` file in `/home/me/Documents`"#,
         ));
     }
 
@@ -28,96 +31,115 @@ fn main() -> Result<(), String> {
         output_file_name
     );
 
-    match std::fs::read(program_path) {
-        Ok(file_contents) => {
-            if let Ok(output_buffer) = std::fs::File::create(&output_path) {
-                let length = file_contents.len();
+    let file_contents = std::fs::read(program_path)
+        .map_err(|err| format!("Failed to open {program_path}: {err}"))?;
+    let file_contents_len = file_contents.len();
 
-                let mut cursor = std::io::Cursor::new(file_contents);
-                let mut writer = std::io::BufWriter::new(output_buffer);
+    let output_buffer = std::fs::File::create(&output_path)
+        .map_err(|err| format!("Failed creating file at {output_path}: {err}"))?;
 
-                while !(cursor.position() == length.try_into().unwrap()) {
-                    if let Ok(opcode) = cursor.read_u16::<BigEndian>() {
-                        let instr = instr::Instruction::from_u16(opcode);
+    let mut cursor = std::io::Cursor::new(file_contents);
+    let mut writer = std::io::BufWriter::new(output_buffer);
 
-                        match instr.msb {
-                            0x0 => {}
-                            0x1 => {
-                                let _ = writer.write_fmt(format_args!(
-                                    "JP ${0:03X?} ; Jumps to address ${0:03X?}\n",
-                                    instr.address
-                                ));
-                            }
-                            0x2 => {
-                                let _ = writer.write_fmt(format_args!(
-                                    "CALL ${0:03X?} ; Jumps to subroutine at ${0:03X?}\n",
-                                    instr.address
-                                ));
-                            }
-                            0x3 => {
-                                let _ = writer.write_fmt(format_args!(
-                                    "SE V{0:X?}, {1:02X?} ; Skip the next instruction if the value in register V{0:X?} is equal to {1:02X?}\n", instr.x, instr.kk_byte
-                                ));
-                            }
-                            0x4 => {
-                                let _ = writer.write_fmt(format_args!(
-                                    "SNE V{0:X?}, {1:02X?} ; Skip the next instruction if the value in register V{0:X?} is not equal to {1:02X?}\n", instr.x, instr.kk_byte
-                                ));
-                            }
-                            0x5 => {
-                                let _ = writer.write_fmt(format_args!(
-                                    "SE V{0:X?}, V{1:X?} ; Skip the next instruction if the values in registers V{0:X?} and V{0:X?} are equal.\n", instr.x, instr.y
-                                ));
-                            }
-                            0x6 => {
-                                let _ = writer.write_fmt(format_args!(
-                                    "LD V{0:X?}, {1:02X?} ; Set the value of register V{0:X?} to {1:02X?}\n", instr.x, instr.kk_byte
-                                ));
-                            }
-                            0x7 => {
-                                let _ = writer.write_fmt(format_args!(
-                                    "ADD V{0:X?}, {1:02X?} ; Add {1:02X?} to the value in register V{0:X?}\n", instr.x, instr.kk_byte
-                                ));
-                            }
-                            0x8 => {
-                                match instr.nibble {
-                                    0x0 => todo!(),
-                                    0x1 => todo!(),
-                                    0x2 => todo!(),
-                                    0x3 => todo!(),
-                                    0x4 => todo!(),
-                                    0x5 => todo!(),
-                                    0x6 => todo!(),
-                                    0x7 => todo!(),
-                                    0xE => todo!(),
-                                    _ => {
-                                        unreachable!(
-                                            "No instruction starting with 0x8 can end with {:X}",
-                                            instr.nibble
-                                        );
-                                    }
-                                };
-                            }
-                            0x9 => todo!(),
-                            0xA => {
-                                let _ = writer.write_fmt(format_args!("LD I, ${0:03X} ;; Load address ${0:03X} into the special register I\n", instr.address));
-                            }
-                            0xB => todo!(),
-                            0xC => todo!(),
-                            0xD => todo!(),
-                            0xE => todo!(),
-                            0xF => todo!(),
-                            _ => {
-                                let _ = writer.write(b"Unknown instruction\n");
-                            }
-                        }
-                    }
+    while !(cursor.position() == file_contents_len.try_into().unwrap()) {
+        if let Ok(opcode) = cursor.read_u16::<BigEndian>() {
+            let instr = instr::Instruction::from_u16(opcode);
+
+            match instr.msb {
+                0x0 => {}
+                0x1 => {
+                    writer
+                        .write_fmt(format_args!(
+                            "JP ${0:03X?} ; Jumps to address ${0:03X?}\n",
+                            instr.address
+                        ))
+                        .unwrap_or_default();
                 }
-            } else {
-                return Err(format!("Failed creating file {}", &output_path));
+                0x2 => {
+                    writer
+                        .write_fmt(format_args!(
+                            "CALL ${0:03X?} ; Jumps to subroutine at ${0:03X?}\n",
+                            instr.address
+                        ))
+                        .unwrap_or_default();
+                }
+                0x3 => {
+                    writer.write_fmt(format_args!(
+                        "SE V{0:X?}, {1:02X?} ;; Skip the next instruction if the value in register V{0:X?} is equal to {1:02X?}\n",
+                        instr.x,
+                        instr.kk_byte
+                    )).unwrap_or_default();
+                }
+                0x4 => {
+                    writer.write_fmt(format_args!(
+                        "SNE V{0:X?}, {1:02X?} ;; Skip the next instruction if the value in register V{0:X?} is not equal to {1:02X?}\n",
+                        instr.x,
+                        instr.kk_byte
+                    )).unwrap_or_default();
+                }
+                0x5 => {
+                    writer.write_fmt(format_args!(
+                        "SE V{0:X?}, V{1:X?} ;; Skip the next instruction if the values in registers V{0:X?} and V{0:X?} are equal.\n",
+                        instr.x,
+                        instr.y
+                    )).unwrap_or_default();
+                }
+                0x6 => {
+                    writer
+                        .write_fmt(format_args!(
+                        "LD V{0:X?}, {1:02X?} ;; Set the value of register V{0:X?} to {1:02X?}\n",
+                        instr.x,
+                        instr.kk_byte
+                    ))
+                        .unwrap_or_default();
+                }
+                0x7 => {
+                    writer
+                        .write_fmt(format_args!(
+                        "ADD V{0:X?}, {1:02X?} ;; Add {1:02X?} to the value in register V{0:X?}\n",
+                        instr.x, instr.kk_byte
+                    ))
+                        .unwrap_or_default();
+                }
+                0x8 => {
+                    match instr.nibble {
+                        0x0 => todo!(),
+                        0x1 => todo!(),
+                        0x2 => todo!(),
+                        0x3 => todo!(),
+                        0x4 => todo!(),
+                        0x5 => todo!(),
+                        0x6 => todo!(),
+                        0x7 => todo!(),
+                        0xE => todo!(),
+                        _ => {
+                            unreachable!(
+                                "No instruction starting with 0x8 can end with {:X}",
+                                instr.nibble
+                            );
+                        }
+                    };
+                }
+                0x9 => todo!(),
+                0xA => {
+                    writer
+                        .write_fmt(format_args!(
+                            "LD I, ${0:03X} ;; Load address ${0:03X} into the special register I\n",
+                            instr.address
+                        ))
+                        .unwrap_or_default();
+                }
+                0xB => todo!(),
+                0xC => todo!(),
+                0xD => todo!(),
+                0xE => todo!(),
+                0xF => todo!(),
+                _ => {
+                    writer.write(b"Unknown instruction\n").unwrap_or_default();
+                }
             }
         }
-        Err(e) => return Err(e.to_string()),
     }
+
     Ok(())
 }
